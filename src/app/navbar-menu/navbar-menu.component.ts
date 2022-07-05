34ca35bb4044623158c65aa0e390fd51ed1d4e00
Neimaad <damienlaoussing@hotmail.com>
2022-07-06 01:41:50 +0200

    add menu and google icon

--- src/app/navbar-menu/navbar-menu.component.ts
+++ src/app/navbar-menu/navbar-menu.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-navbar-menu',
   templateUrl: './navbar-menu.component.html',
   styleUrls: ['./navbar-menu.component.scss']
 })
-export class NavbarMenuComponent implements OnInit {
+export class NavbarMenuComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  showFiller = false;
 
 }
